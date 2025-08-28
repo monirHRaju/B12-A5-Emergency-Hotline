7671465b7e4b4a16703a663d671f2f0a791027fd
monirHRaju <monirhraju@gmail.com>
2025-08-28 17:52:13 +0600

    design part done, heart count done.

--- scrpts/script.js
+++ scrpts/script.js
@@ -1,1 +1,50 @@
 // universal scripts
+console.log('script  connected')
+//re usable functions
+function getValue(id){
+    const value = document.getElementById(id).value;
+    
+    return value;
+}
+function getValueNumber(id){
+    const value = document.getElementById(id).value;
+    const valueNumber = parseInt(value);
+
+    return valueNumber;
+}
+function getInnerTextNumber(id){
+    const innerText = document.getElementById(id).innerText;
+    const innerTextNumber = parseInt(innerText);
+
+    return innerTextNumber;
+}
+
+function setAvailableBalance(value){
+    const availableBalance = document.getElementById('balance');
+    
+    availableBalance.innerText = value;
+}
+
+function getElement(id){
+    const element = document.getElementById(id)
+    return element;
+}
+
+function getElements(className){
+    const elements = document.getElementsByClassName(className)
+    return elements;
+}
+
+
+
+
+//main functions
+let count = 0;
+const heartButtons = document.querySelectorAll('.fa-heart');
+for(const button of heartButtons){
+    button.addEventListener('click', function(){
+        count++;
+        getElement('count').innerText = count;
+    })
+}
+
